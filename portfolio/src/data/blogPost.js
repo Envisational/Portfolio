86a1b1994eca090ac43874c04e435a170e43af90
Envisational <insight.da101@gmail.com>
2024-08-20 16:17:27 +0300

    Blog post data in data directory created (bug with showing posts exists)

---
+++ portfolio/src/data/blogPost.js
@@ -0,0 +1,11 @@
+// Will be fetched from a server as JSON late
+const blogPosts = [
+    {
+      id: 1,
+      title: 'My Transition from an English teacher to a Software Developer',
+      date: 'August 18, 2024',
+      content: "In this post, I share how I transitioned from a background in English Literature to becoming a Backend Developer..."
+    }
+  ];
+
+  export default blogPosts;
