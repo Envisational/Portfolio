495573de7b32a035c29f2d5df424a725b16edff1
Envisational <insight.da101@gmail.com>
2024-08-20 16:58:04 +0300

    Post styling added

--- portfolio/src/components/BlogPost.js
+++ portfolio/src/components/BlogPost.js
@@ -17,7 +17,10 @@ const BlogPost = () => {
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-bold mb-4">{post.title}</h2>
         <p className="text-gray-600 mb-8">{post.date}</p>
-        <p className="text-gray-700 leading-relaxed">{post.content}</p>
+        <div
+          className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg text-gray-800 blog-content"
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
       </div>
     </section>
   );
--- portfolio/src/data/blogPost.js
+++ portfolio/src/data/blogPost.js
@@ -1,10 +1,37 @@
-// Will be fetched from a server as JSON late
+// Will be fetched from a server as JSON later
 const blogPosts = [
     {
       id: 1,
       title: 'My Transition from an English teacher to a Software Developer',
       date: 'August 18, 2024',
-      content: "In this post, I share how I transitioned from a background in English Literature to becoming a Backend Developer..."
+      content: `
+      <p>I've always LOVED tech! Until eventually stuff breaks down and you start reflecting on your life decisions :) </p>
+    
+      <p>I don't come from a traditional CS background but that seems to be the norm these days! I was born in Ukraine but I grew up in the US and Iran.
+      I started out with a degree in English Literature back in college and wanted to become a teacher. And I did!
+      But I also wanted more. Coding and SWE in any capacity has an unbelievable rush and feeling like no other.</p>
+
+      <p>I had somewhat of a soft transition into tech when I took Harvard's <a href="https://cs50.harvard.edu/x/2024/">CS50</a> back in 2020. And ever since then, I just made things!
+      I strongly advocate for persistence, meaning that if you spend enough time doing anything, you're gonna become good at that said thing IF YOU TRY TO LEARN FROM PEOPLE WHO ARE ALREADY GOOD AT IT!</p>
+
+      <p>The first real project I ever worked on was <a href="https://en.qom.ac.ir/">this university website</a> where I, along with a couple of like-minded individuals who hated sleep, worked on improving the most horrible website ever.</p>
+      It was challenging, tough, and I had to learn more than I did in my entire life long before tools like AI were a thing!
+
+      But this taught me that the field of SWE is all about the same cycle of learning something new to solve a problem then doing it over and over again until you become comfortable with being uncomfortable.
+
+      <p>Since then, I put myself out there and have worked as a contractor/freelancer for clients of all sizes while continuing to teach English for <a href="https://www.camelot-eng.com/?lang=en">Camelot English School</a>.
+
+      I worked on many projects. Some more exciting, some less. Building/improving web applications in different sectors like medicine, e-commerce, and even entertainment!
+      Life was good but definitely not reflective of "work-life balance", and there was also something missing.</p> 
+
+      <p>In mid-2023 however, I received a surprise call from one of my old friends from college who's in the States.
+      He had a brilliant idea for a startup and wanted me to join! I was literally shaking because I knew what it meant: I had to go all in.</p>
+
+      <p>Gone were the days of teaching English to people, and in were the days of rapid development with pristine precision, and the occasional why isn't this working.
+      Over the past year, I've gained enough experience working closely with our backend team to deliver something I can say I'm proud of!</p>
+
+      It's now August 2024 as of writing this, and I have a feeling that it's just the beginning for me.
+    `
     }
   ];
 
